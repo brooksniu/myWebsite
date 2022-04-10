0bd38402ba4272ffb35d6f24faea3290cdafe6d0
Ruijia Brooks Niu <brooksnew92@gmail.com>
2022-04-09 22:28:08 -0700

    updated status; in progress

--- page.js
+++ page.js
@@ -1,1 +1,31 @@
-window.addEventListener("DOMContentloaded", init);
+window.addEventListener("DOMContentLoaded", init);
+
+async function init() {
+    // navbar elements
+    let projects = document.getElementById("projects");
+    let work = document.getElementById("work");
+    let contact = document.getElementById("contact");
+    // add eventlistener to react
+    projects.addEventListener("mouseover", async function() {
+        projects.textContent = "Site in progress";
+    });
+    projects.addEventListener("mouseout", async function() {
+        projects.textContent = "Projects";
+    });
+
+    work.addEventListener("mouseover", async function() {
+        work.textContent = "Site in progress";
+    });
+    work.addEventListener("mouseout", async function() {
+        work.textContent = "work";
+    });
+
+    contact.addEventListener("mouseover", async function() {
+        contact.textContent = "Site in progress";
+    });
+    contact.addEventListener("mouseout", async function() {
+        contact.textContent = "contact";
+    });
+}
+
+
